8aeea936aa85e661ec13313a27d14b1d64b76cf5
ptrwping <pingptrw2002@hotmail.com>
2022-09-23 16:28:48 +0700

    modified bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("WHAT IS YOUR NAME? ");
     scanf("%s", Name);
     do
     {
-      printf("What is your blood group [A, B, AB, or O]? ");
+      printf("WHAT IS YOUR BLOOD GROUP"" [A, B, AB, or O]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
